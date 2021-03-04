356a4fc68f855d42be5abd521f9d341514cdf014
Spirit5736 <kozhevnikovi@mail.ru>
2021-03-04 23:27:45 +0700

    Added Model

---
+++ Model/Formula.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Calculation_of_functions
+{
+    public class Formula : INotifyPropertyChanged
+    {
+        private string title;
+        private string formulaOfFunction;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        public string FormulaOfFunction
+        {
+            get { return formulaOfFunction; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("formulaOfFunction");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
